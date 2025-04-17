7787aa6a04910ba2e3c9ccba8f6c3346d18c0926
Eziz <zeehallyyev@gmail.com>
2025-04-16 22:31:49 -0500

    adding new code from HW_Informatics

--- src/InformaticsHw/C/cutApple2.java
+++ src/InformaticsHw/C/cutApple2.java
@@ -1,12 +1,13 @@
 package InformaticsHw.C;
 
 import java.util.Scanner;
+
 public class cutApple2 {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        int x = k%n;
-        System.out.println(x);
+        int b = k % n;
+        System.out.println(b);
     }
 }
